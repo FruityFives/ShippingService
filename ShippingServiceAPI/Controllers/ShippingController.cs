d12329f0318e5dae9b8b7a7b575bf820c1684ab2
Fawad Nassery <eaa23fana@students.eaaa.dk>
2025-04-07 11:42:49 +0200

    FawadTest

--- ShippingServiceAPI/Controllers/ShippingController.cs
+++ ShippingServiceAPI/Controllers/ShippingController.cs
@@ -57,7 +57,7 @@ namespace ShippingServiceAPI.Controllers
         }
 
 
-        //HudayfaTest
+        //FawadTest
         [HttpPost]
         public async Task<IActionResult> ShipOrder([FromBody] OrderDTO order)
         {
